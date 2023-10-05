f542ce09d5be6e2d0aa57bd695d81b645a5f4cb6
Braham Aggarwal <brahamaggarwal739625@gmail.com>
2023-10-05 22:22:28 +0530

    Seperating mqtt and API

--- server.js
+++ server.js
@@ -22,40 +22,39 @@ const DB = process.env.DATABASE.replace(
 );
 
 // TODO - Need to restructure code
-const mqttConnectWithCycleDB = () => {
-  const options = {
-    host: process.env.MQTT_HOST,
-    port: process.env.MQTT_PORT,
-  };
+// const mqttConnectWithCycleDB = () => {
+//   const options = {
+//     host: process.env.MQTT_HOST,
+//     port: process.env.MQTT_PORT,
+//   };
 
-  const client = mqtt.connect(options);
+//   const client = mqtt.connect(options);
 
-  client.on('connect', () => {
-    client.subscribe('esp/humidity');
-    // client.subscribe('XOXO');
-    console.log('MQTT client has subscribed');
-  });
-  let flag = false;
-  client.on('message', async (topic, message) => {
-    message = message.toString().split(',');
-    message[1] = +message[1];
-    message[2] = +message[2];
-    if (flag === true) return;
-    flag = true;
-    const updateLocation = {
-      $set: {
-        'location.coordinates': [message[1], message[2]],
-      },
-    };
+//   client.on('connect', () => {
+//     client.subscribe('esp/humidity');
+//     console.log('MQTT client has subscribed');
+//   });
+//   let flag = false;
+//   client.on('message', async (topic, message) => {
+//     message = message.toString().split(',');
+//     message[1] = +message[1];
+//     message[2] = +message[2];
+//     if (flag === true) return;
+//     flag = true;
+//     const updateLocation = {
+//       $set: {
+//         'location.coordinates': [message[1], message[2]],
+//       },
+//     };
 
-    await Cycle.updateOne({ name: `TempCycle-${message[0]}` }, updateLocation);
-    // console.log(message);
-  });
+//     await Cycle.updateOne({ name: `TempCycle-${message[0]}` }, updateLocation);
+//     // console.log(message);
+//   });
 
-  setInterval(() => {
-    flag = false; // Reset the flag to false
-  }, process.env.MQTT_UPDATE_INTERVAL);
-};
+//   setInterval(() => {
+//     flag = false; // Reset the flag to false
+//   }, process.env.MQTT_UPDATE_INTERVAL);
+// };
 
 mongoose
   .connect(DB, {
@@ -64,7 +63,7 @@ mongoose
   })
   .then(() => {
     console.log('DB connection successful!');
-    mqttConnectWithCycleDB(); //! This is a funcking mess, make seperate file for this functionality (Maybe a seperate code and then a bash script to orchestrate all of this)
+    // mqttConnectWithCycleDB(); //! This is a funcking mess, make seperate file for this functionality (Maybe a seperate code and then a bash script to orchestrate all of this)
   });
 
 const port = process.env.PORT;
